56bbb8b9bffc0a578da13e02ea8a0a68e57fa413
luozhouyang <zhouyang.luo@gmail.com>
2021-11-19 23:09:26 +0800

    Bump version to 0.0.2

--- smile_datasets/__init__.py
+++ smile_datasets/__init__.py
@@ -4,8 +4,11 @@ from smile_datasets.dataset import Datapipe
 from smile_datasets.qa.dataset import DatapipeForQuestionAnswering, DatasetForQuestionAnswering
 from smile_datasets.qa.example import ExampleForQuestionAnswering
 from smile_datasets.qa.parsers import ParserForQuestionAnswering
+from smile_datasets.sequence_classification.dataset import DatapipeForSequenceClassifiation, DatasetForSequenceClassification
+from smile_datasets.sequence_classification.example import ExampleForSequenceClassification
+from smile_datasets.sequence_classification.parsers import ParserForSequenceClassification
 
 __name__ = "smile_datasets"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s", level=logging.INFO)
