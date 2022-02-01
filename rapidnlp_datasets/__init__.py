dc483b73218ffeca92d119779bcfb85c1c77d730
luozhouyang <zhouyang.luo@gmail.com>
2022-02-01 17:09:13 +0800

    Bump version to 0.2.0

--- rapidnlp_datasets/__init__.py
+++ rapidnlp_datasets/__init__.py
@@ -7,11 +7,12 @@ from .masked_lm import (
     ExampleForMaskedLanguageModel,
     WholeWordMaskingForLanguageModel,
 )
+from .question_answering import DatasetForQuestionAnswering, ExampleForQuestionAnswering
 from .sequence_classification import DatasetForSequenceClassification, ExampleForSequenceClassification
 from .simcse import DatasetForSimCSE, ExampleForSimCSE
 from .token_classification import DatasetForTokenClassification, ExampleForTokenClassification
 
 __name__ = "rapidnlp_datasets"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s", level=logging.INFO)
